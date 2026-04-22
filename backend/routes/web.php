ba514f90e1283da48bfcb44d0f898a6401a7bb9e
Ethan Maestas <ethanmdev@gmail.com>
2026-04-22 14:50:39 -0600

    testing

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,14 +1,2 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
-Route::get('/{any?}', function () {
-    $path = public_path('index.html');
-
-    if (! file_exists($path)) {
-        abort(404, 'SPA not built yet. Run build.sh to generate the frontend.');
-    }
-
-    return response(file_get_contents($path))
-        ->header('Content-Type', 'text/html');
-})->where('any', '^(?!api|sanctum).*$');
